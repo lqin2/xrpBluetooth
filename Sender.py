2d2fd41023d69397e15390d95078d19f1524caf8
lqin2 <148113184+lqin2@users.noreply.github.com>
2025-07-01 18:05:08 -0700

    Update Sender.py

--- Sender.py
+++ Sender.py
@@ -7,8 +7,8 @@ while True:
     pot_value = potentiometer.read_u16()
     angle = (pot_value / 65535) * 180
     min_duty = 1000
-    max_duty = 9000
+    max_duty = 5000
     duty = int(min_duty + (max_duty - min_duty) * angle / 180)
     print(f"DUTY:{duty}")  # ✅ Sent to your PC via USB (COM8)
-    time.sleep(0.5)
+    time.sleep(0.05)
 
